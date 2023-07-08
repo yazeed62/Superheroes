e4ebd7a49c7489af4ccab9a687a2bcb372d9edbf
Yazeed <dee3ayahmed25@gmail.com>
2023-07-08 22:51:26 +0300

    defined create in heropowercontroller

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,18 @@
 class HeroPowersController < ApplicationController
+   def create
+    hero = Hero.find_by(id: params[:hero_id])
+    power = Power.find_by(id: params[:power_id])
+
+    if hero && power
+      hero_power = HeroPower.new(hero: hero, power: power, strength: params[:strength])
+      if hero_power.save
+        render json: hero, only: [:id, :name, :super_name], include: { powers: { only: [:id, :name, :description] } }
+      else
+        render json: { errors: hero_power.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: 'Hero or Power not found' }, status: :not_found
+    end
+  end
 end
+

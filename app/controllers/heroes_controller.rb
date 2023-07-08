eeb252a783f76f7a430c37666782e93f98583940
Yazeed <dee3ayahmed25@gmail.com>
2023-07-08 22:48:10 +0300

    defined index and show in heroescontroller

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,2 +1,16 @@
 class HeroesController < ApplicationController
+   def index
+    heroes = Hero.all
+    render json: heroes, only: [:id, :name, :super_name]
+  end
+
+  def show
+    hero = Hero.find_by(id: params[:id])
+    if hero
+      render json: hero, only: [:id, :name, :super_name], include: { powers: { only: [:id, :name, :description] } }
+    else
+      render json: { error: 'Hero not found' }, status: :not_found
+    end
+  end
+end
 end

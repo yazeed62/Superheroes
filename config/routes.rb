5cac6d3b54070a408daef90587e8c144907a275e
Yazeed <dee3ayahmed25@gmail.com>
2023-07-08 22:33:20 +0300

    defined routes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :heroes, only: [:index, :show]
+  resources :powers, only: [:index, :show, :update]
+  resources :hero_powers, only: [:create]
 end

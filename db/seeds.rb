37af0fa58a606959334bd8d9fdce5e3d2f64eb99
Yazeed <dee3ayahmed25@gmail.com>
2023-07-10 12:13:54 +0300

    update in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+power.delete_all
+Hero.delete_all
+
+Hero.create(name: "Kamala Khan", super_name: "Ms. Marvel")
+Hero.create(name: "Doreen Green", super_name: "Squirrel Girl")
+Hero.create(name: "Gwen Stacy", super_name: "Spider-Gwen")
+
+heros.each do |hero_data|
+  hero = Hero.create(hero_data)
+  hero.powers << Power.all.sample(2)
+ends
+
+puts "Seed data for heroes created successfully"
+
+powers = [
+  { name: "super strength", description: "gives the wielder super-human strengths" },
+  { name: "flight", description: "gives the wielder the ability to fly through the skies at supersonic speed" }
+]
+
+powers.each do |power_data|
+  Power.create(power_data)
+end
+
+puts "Seed data for powers created successfully"
